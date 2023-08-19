1a6df2862f81792851ebba0840ccf5cd51936667
Simston <32360996+Simston@users.noreply.github.com>
2023-08-19 12:21:13 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,48 @@
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+const toggleIcon = document.getElementById('toggle-icon');
+const nav = document.getElementById('nav');
+const image1 = document.getElementById('image1');
+const image2 = document.getElementById('image2');
+const image3 = document.getElementById('image3');
+const textBox = document.getElementById('text-box');
+
+// Dark Or Light Images
+function imageMode(color) {
+    image1.src = `img/science_${color}.svg`;
+    image2.src = `img/aircraft_${color}.svg`;
+    image3.src = `img/world_${color}.svg`;
+}
+
+// Dark Mode Styles
+function darkMode() {
+    nav.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+    textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+    toggleIcon.children[0].textContent = 'Dark Mode';
+    toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon');
+    imageMode('dark');
+}
+
+// Light Mode
+function lightMode() {
+    nav.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+    textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+    toggleIcon.children[0].textContent = 'Light Mode';
+    toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+    imageMode('light');
+}
+
+// Switch Theme Dynamically
+function switchTheme(event) {
+    if (event.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+        darkMode();
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+        lightMode();
+    }
+}
+
+// Event Listener
+toggleSwitch.addEventListener('change', switchTheme);
